aea211f09d20cdf0a1952950667582d5d04ba5b2
Rod Rodriguez <rodriguezrod1@gmail.com>
2024-04-07 16:30:47 -0400

    Change code validation 400 to 422

--- app/Http/Requests/BaseFormRequest.php
+++ app/Http/Requests/BaseFormRequest.php
@@ -35,6 +35,6 @@ class BaseFormRequest extends FormRequest
             'success'   => false,
             'message'   => 'Validation errors',
             'data'      => $validator->errors()
-        ], 400));
+        ], 422));
     }
 }
