1cbc8c723e14b93ed6a366569dad99971267bc03
Rod Rodriguez <rodriguezrod1@gmail.com>
2024-04-08 06:16:55 -0400

    Change name api web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Doctor-Salazar-api: ' => app()->version()];
+    return ['Medical Patients API: ' => app()->version()];
 });
